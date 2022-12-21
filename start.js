f5878b2600be9ac980c0252dfea9f90eecc5caa3
ggranado <ggranado@3dlinkweb.com>
2022-12-21 00:33:38 -0400

    fix packages

--- start.js
+++ start.js
@@ -1,1 +1,14 @@
+const app = require("./server");
+const mongo = require("./mongo.js");
 
+(async (arg1, arg2, arg3) => {
+  await mongo().then((mongoose) => {
+    try {
+      app.listen(process.env.PORT || 8000, () => {
+        console.log("Server started.");
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  });
+})();
